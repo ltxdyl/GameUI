714dd1d1ed95eb0b7df4b3e6ba494f33357a8366
ltxdyl <sldkfja@163.com>
2017-09-06 23:45:43 +0800

    add public and menu ui

---
+++ D:\Laya\Code\GameClient\src\UI/Battle/BattleBinder.ts
@@ -0,0 +1,10 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+module Battle {
+
+	export class BattleBinder{
+		public static bindAll():void {
+			fairygui.UIObjectFactory.setPackageItemExtension(UIWndBattle.URL, UIWndBattle);
+		}
+	}
+}
